0dd17e6808fb3d2efaa3a6f2fd2b5d86faf07372
iida hideaki <hideaki.19790727@gmail.com>
2026-05-03 15:21:09 +0900

    Refactor UserController

--- UserController.php
+++ UserController.php
@@ -4,21 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::pagenate(20);
         return view('users.index', ['users' => $users]);
     }
 
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required | max:50',
+            'email' => 'required | email | unique:users',
+            'password' => 'required | min:8',
+        ]);
         $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
+        $user->name = $validated['name'];
+        $user->email = $validated['email'];
+        $user->password = Hash::make($validated['password']);
         $user->save();
 
         return redirect('/users');
